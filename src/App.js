a31d2c86f4b8377afd8441f60dafaf48558655ff
Huzaifa <huzaifaahxan6@gmail.com>
2022-11-09 10:43:19 -0800

    monster-rolodex

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
     )
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>Monsters Rolodex App</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
