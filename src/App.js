ed2a71445f17ee82bb31b89e32729b11f2b9f8f4
Huzaifa <huzaifaahxan6@gmail.com>
2022-11-09 10:32:28 -0800

    heading name monsters rolodex

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default class App extends React.Component {
     )
     return (
       <div className="App">
-        <h1>Rolodex</h1>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
